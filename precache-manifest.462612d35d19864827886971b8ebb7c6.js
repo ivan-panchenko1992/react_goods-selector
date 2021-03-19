8ae47f5bb770b6048936139e39520edf467d953f
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-19 10:14:30 +0200

    Updates

---
+++ precache-manifest.462612d35d19864827886971b8ebb7c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf3ce46d838ee7bcfffebc24230d781",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f0e45cae97b3ce4781d",
+    "url": "./static/css/main.2d56192c.chunk.css"
+  },
+  {
+    "revision": "c07500df0ccde94f4968",
+    "url": "./static/js/2.f3cc3ca8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f3cc3ca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0e45cae97b3ce4781d",
+    "url": "./static/js/main.4c382814.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
